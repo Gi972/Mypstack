270ec602e9c81179b67067286d053e702dfdef7c
Jean-Christophe DEMAR <j3di.net@gmail.com>
2018-02-18 19:26:09 +0100

    init stack

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+const App = () => <div>{"Hello Stack"}</div>;
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <div className="container">
+    <App />
+  </div>,
+  document.getElementById("root")
+);
